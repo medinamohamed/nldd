c3572d2bf28fc586716e98cedeb2453192dfa18a
Medina <medina.ahmed.mo@gmail.com>
2022-05-03 10:33:36 -0400

    update

--- script/global_xy.py
+++ script/global_xy.py
@@ -1,6 +1,7 @@
 
 import pandas as pd
 import math
+import json
 
 df = pd.read_table('nldd_classe.tsv')
 
@@ -20,7 +21,35 @@ p0 = referencePoint(0, 0, 45.590231380357075, -73.68553872266351)
 p1 = referencePoint(2244, 2060, 45.55443164693175, -73.59927931059248)
 
 
+# def getScreenXY(pos_x,pos_y):
+    
+#     p0_pos_ref = latlngToGlobalXY(p0.lat,p0.lng)
 
+#     p1_pos_ref = latlngToGlobalXY(p1.lat,p1.lng)
+
+#     perX = ((pos_x-p0_pos_ref['x'])/(p1_pos_ref['x'] -p0_pos_ref['x'] ))
+
+#     perY = ((pos_y-p0_pos_ref['y'])/(p1_pos_ref['y'] -p0_pos_ref['y'] ))
+
+#     screen_x = p0.scrX + (p1.scrX - p0.scrX)*perX
+#     screen_y = p0.scrY + (p1.scrY - p0.scrY)*perY
+
+#     return {'x': screen_x/10, 'y': screen_y/10}
+
+def getScreenX(pos_x):
+
+    p0_pos_ref = latlngToGlobalXY(p0.lat,p0.lng)
+
+    p1_pos_ref = latlngToGlobalXY(p1.lat,p1.lng)
+
+    perX = ((pos_x-p0_pos_ref['x'])/(p1_pos_ref['x'] -p0_pos_ref['x'] ))
+
+    screen_x = p0.scrX + (p1.scrX - p0.scrX)*perX
+
+    return screen_x/10
+
+
+    
 
 def main():
 
@@ -32,35 +61,28 @@ def main():
     df['global_x'] = global_x
     df['global_y'] = global_y
 
-    pos_a_x = global_x[1]
-    pos_a_y = global_y[1]
+    dict = getAllScreenXY(global_x,global_y)
 
-    p0_pos_ref = latlngToGlobalXY(p0.lat,p0.lng)
+    # use json.dump to write the file
+    with open('./screenXY.json', 'w') as file:
+        json.dump(dict, file, indent=4)
 
-    p1_pos_ref = latlngToGlobalXY(p1.lat,p1.lng)
-
-    perX = ((pos_a_x-p0_pos_ref['x'])/(p1_pos_ref['x'] -p0_pos_ref['x'] ))
-
-    perY = ((pos_a_y-p0_pos_ref['y'])/(p1_pos_ref['y'] -p0_pos_ref['y'] ))
-
-    test_x = p0.scrX + (p1.scrX - p0.scrX)*perX
-    test_y = p0.scrY + (p1.scrY - p0.scrY)*perY
-
-    print(test_x,test_y)
-
-    # print("pos_a_x", pos_a_x)
-
-    # print("p0_pos_ref['x']", p0_pos_ref['x'])
-
-    # print("(p1_pos_ref['x']", p1_pos_ref['x'])
-
-    # print("p0_pos_ref['x']", p0_pos_ref['x'])
+    
 
 
+def getAllScreenXY(global_x,global_y):
 
-    # df.to_json(r'nldd_xy_classe.json')
+    dict = {}
 
+    for i in range(0,len(global_x)):
 
+        index = 'point' + str(i)
+
+        test = getScreenXY(global_x[i],global_y[i])
+
+        dict[index] = test
+
+    return dict
 
 # This function converts lat and lng coordinates to GLOBAL X and Y positions
 def latlngToGlobalXY(lat, lng):
